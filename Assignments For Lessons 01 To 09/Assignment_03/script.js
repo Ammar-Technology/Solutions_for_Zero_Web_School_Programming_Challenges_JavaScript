14e3388c137ca623318abf3f6fc074971a6666e2
Ammar Immad <126868193+Ammar-Technology@users.noreply.github.com>
2026-07-07 04:48:18 +0300

    Fix console log styles in script.js

--- Assignments For Lessons 01 To 09/Assignment_03/script.js
+++ Assignments For Lessons 01 To 09/Assignment_03/script.js
@@ -9,5 +9,5 @@ let Size = "font-size: 40px;";
 
 console.log("%cElzero %cWeb %cSchool",
     `${Size} color: red`,
-    `${Size} 40px; color: green; font-weight: Bold`,
-    `${Size} 40px; color: white; background-color: blue`);
+    `${Size} color: green; font-weight: Bold`,
+    `${Size} color: white; background-color: blue`);
