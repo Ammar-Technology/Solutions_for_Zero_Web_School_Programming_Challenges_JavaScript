eef44b19d9ef11512099d8ee615e8150020f71f4
Ammar Immad <126868193+Ammar-Technology@users.noreply.github.com>
2026-07-07 04:51:37 +0300

    Implement console and document write examples
    Added console and document write examples in script.js.

--- Assignments For Lessons 01 To 09/Assignment_06/script.js
+++ Assignments For Lessons 01 To 09/Assignment_06/script.js
@@ -1,7 +1,13 @@
-console.log = function () { };
-document.write = function () { };
-
-// Advanced solution
+// ========== Solution One ==========
+console.log = function () {};
+document.write = function () {};
 
 console.log("Iam In Console");
 document.write("Iam In Page");
+
+// ========== Solution Two ==========
+
+/*
+console.log("Iam In Console");
+document.write("Iam In Page");
+*/
