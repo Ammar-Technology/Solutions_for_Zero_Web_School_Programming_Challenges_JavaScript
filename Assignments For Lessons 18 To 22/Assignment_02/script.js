e564cd93aa567a923c7e8cdd8e6fc1400ae36f9a
Ammar Immad <126868193+Ammar-Technology@users.noreply.github.com>
2026-07-07 04:38:42 +0300

    Create script.js

---
+++ Assignments For Lessons 18 To 22/Assignment_02/script.js
@@ -0,0 +1,19 @@
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num * num - num); // 6
+
+// Solution Three
+console.log(num + num + true - true); // 6
+
+// Solution Four
+console.log(num << true); // 6
+
+// Solution Five
+console.log(num ** (true + true) - num); // 6
+
+// Solution Six
+console.log("num".length + num); // 6
