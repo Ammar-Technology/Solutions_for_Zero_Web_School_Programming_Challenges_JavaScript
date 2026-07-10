cc5cf795b08a704499dadc33b19ff988f32c9509
Ammar Immad <126868193+Ammar-Technology@users.noreply.github.com>
2026-07-10 19:27:45 +0300

    Implement solutions for number challenge
    Added various methods to compute 100000 in script.js.

---
+++ Assignments For Lessons 23 To 26/Assignment_01/script.js
@@ -0,0 +1,32 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(10 * 10000); // 100000
+
+console.log(Math.pow(10, 5)); // 100000
+
+console.log(10 ** 5); // 100000
+
+console.log(1e5); // 100000
+
+console.log(+("1" + "0".repeat(5))); // 100000
+
+console.log(3125 << 5); // 100000
+
+console.log(100005 - 5); // 100000
+
+console.log(200000 / 2); // 100000
+
+console.log(0x186a0); // 100000
+
+console.log(0b11000011010100000); // 100000
+
+console.log(Number([1, 0, 0, 0, 0, 0].join(""))); // 100000             
+
+console.log(parseInt("100000", 10)); // 100000
+
+console.log(100 * 100 * 10); // 100000
+
